103708749cc047a5e16df718fd3220412da6a536
Ruturajpal <98735716+Ruturajpal@users.noreply.github.com>
2023-12-08 10:33:01 +0530

    Add files via upload

---
+++ src/QuizServiceApplication.java
@@ -0,0 +1,14 @@
+package com.QuizService.QuizService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEureka
+public class QuizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizServiceApplication.class, args);
+	}
+
+}
